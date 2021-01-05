c7780a51cbd422899a524aad041e5ab4f1605c16
VitorCezila <vcezila@gmail.com>
2021-01-05 18:39:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CurrencyConverter-API
+ Commit of the application containing only the conversion function using API. Missing the front-end part
---
+++ app/src/main/java/com/example/currencyconversorapi/MainActivity.kt
@@ -0,0 +1,142 @@
+package com.example.currencyconversorapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.lang.Exception
+import java.lang.String.format
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    var baseCurrency = "EUR"
+    var convertedToCurrency = "USD"
+    var conversionRate = 0f
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        spinnerSetup()
+        textChanged()
+    }
+
+    private fun textChanged() {
+        et_firstConversion.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                try {
+                    getApiResult()
+                } catch (e: Exception) {
+                    Log.e("Main", "$e")
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("Main", "Before Text Changed")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("Main", "On Text Changed")
+            }
+
+        })
+    }
+
+    private fun getApiResult() {
+        if(et_firstConversion != null && et_firstConversion.text.isNotEmpty() && et_firstConversion.text.isNotBlank()) {
+
+            val API = "https://api.exchangeratesapi.io/latest?base=$baseCurrency&symbols=$convertedToCurrency"
+
+            if(baseCurrency == convertedToCurrency) {
+                Toast.makeText(applicationContext, "Cannot convert the same currency", Toast.LENGTH_SHORT).show()
+            } else {
+                GlobalScope.launch(Dispatchers.IO) {
+
+                    try {
+                        val apiResult = URL(API).readText()
+                        val jsonObject = JSONObject(apiResult)
+
+                        conversionRate = jsonObject.getJSONObject("rates").getString(convertedToCurrency).toFloat()
+
+                        Log.d("Main","$conversionRate")
+                        Log.d("Main", apiResult)
+
+                        withContext(Dispatchers.Main) {
+                            val text = ((et_firstConversion.text.toString().toFloat()) * conversionRate).toString()
+                            et_secondConversion?.setText(text) //update in real time the edit text
+                        }
+                    } catch (e: Exception) {
+                        Log.e("Main", "$e")
+                    }
+                }
+            }
+
+        }
+    }
+
+    private fun spinnerSetup() {
+        val spinner1: Spinner = findViewById(R.id.spinner_firstConversion)
+        val spinner2: Spinner = findViewById(R.id.spinner_secondConversion)
+
+        //first spinner adapter from strings.xml items
+        ArrayAdapter.createFromResource(this, R.array.currencies1, android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner1.adapter = adapter
+        }
+
+        //second spinner adapter from strings.xml items
+        ArrayAdapter.createFromResource(this, R.array.currencies2, android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner2.adapter = adapter
+        }
+
+        spinner1.onItemSelectedListener = (object: AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                baseCurrency = parent?.getItemAtPosition(position).toString()
+                getApiResult()
+            }
+
+        })
+
+        spinner2.onItemSelectedListener = (object: AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                convertedToCurrency = parent?.getItemAtPosition(position).toString()
+                getApiResult()
+            }
+
+        })
+
+    }
+
+}
